0c1ea7d60c56291072f469dc061df9d9f96734a0
Jose Juan Moñino Coll <36202313+monyinet@users.noreply.github.com>
2022-12-23 23:08:04 +0000

    install and set multer, add API post route and finish project

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +12,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer({ dest: 'uploads/' });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({ 
+		name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+	});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
